2c18dad1d64cbd3b17183ffe22cb41e4e0fd4da0
shkirya.ns <shkirya.ns@oilpc.ru>
2023-12-15 21:07:52 +0600

    refactored vosk-client

--- pkg/vosk-client/vosk-client.go
+++ pkg/vosk-client/vosk-client.go
@@ -11,6 +11,10 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var (
+	log logger = _l{}
+)
+
 func getenv(key string, d string) string {
 	v := os.Getenv(key)
 	if len(v) == 0 {
@@ -25,22 +29,32 @@ type logger interface {
 	DEBUG(v ...string)
 }
 
+type _l struct{}
+
+func (l _l) ERROR(v ...string) {
+	fmt.Println(v)
+}
+func (l _l) INFO(v ...string) {
+	fmt.Println(v)
+}
+func (l _l) DEBUG(v ...string) {
+	fmt.Println(v)
+}
+
 type Client struct {
 	Host       string
 	Port       string
 	buffsize   int
 	SampleRate int
-	log        logger
 	socket     *websocket.Conn
 }
 
-func New(log logger) *Client {
+func New() *Client {
 	clt := &Client{
 		Host:       getenv("VOSK_HOST", "localhost"),
 		Port:       getenv("VOSK_PORT", "2700"),
 		buffsize:   8000,
 		SampleRate: 16000,
-		log:        log,
 	}
 
 	return clt
@@ -58,18 +72,18 @@ type Message struct {
 
 var m Message
 
-func (c *Client) SetLogger(log logger) {
-	c.log = log
+func (c *Client) SetLogger(l logger) {
+	log = l
 }
 
 func (c *Client) PostConfigure() error {
 	u := url.URL{Scheme: "ws", Host: c.Host + ":" + c.Port, Path: ""}
-	c.log.DEBUG("connecting to ", u.String())
+	log.DEBUG("connecting to ", u.String())
 
 	// Opening websocket connection
 	soc, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		c.log.ERROR(err.Error())
+		log.ERROR(err.Error())
 		return err
 	}
 	c.socket = soc
@@ -77,19 +91,19 @@ func (c *Client) PostConfigure() error {
 
 	err = soc.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("{\"config\" : { \"sample_rate\" : %v } }", c.SampleRate)))
 	if err != nil {
-		c.log.ERROR(err.Error())
+		log.ERROR(err.Error())
 		return err
 	}
 
 	// err = c.socket.WriteMessage(websocket.TextMessage, []byte("{\"eof\" : 1}"))
 	// if err != nil {
-	// 	c.log.ERROR(err.Error())
+	// 	log.ERROR(err.Error())
 	// 	return err
 	// }
 	// Closing websocket connection
 	err = c.socket.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 	if err != nil {
-		c.log.ERROR(err.Error())
+		log.ERROR(err.Error())
 		return err
 	}
 
@@ -98,12 +112,12 @@ func (c *Client) PostConfigure() error {
 
 func (c *Client) Recognize(bufWav []byte) (string, error) {
 	u := url.URL{Scheme: "ws", Host: c.Host + ":" + c.Port, Path: ""}
-	c.log.DEBUG("connecting to ", u.String())
+	log.DEBUG("connecting to ", u.String())
 
 	// Opening websocket connection
 	soc, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		c.log.ERROR(err.Error())
+		log.ERROR(err.Error())
 		return "", err
 	}
 	c.socket = soc
@@ -111,7 +125,7 @@ func (c *Client) Recognize(bufWav []byte) (string, error) {
 
 	err = soc.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("{\"config\" : { \"sample_rate\" : %v } }", c.SampleRate)))
 	if err != nil {
-		c.log.ERROR(err.Error())
+		log.ERROR(err.Error())
 		return "", err
 	}
 
@@ -124,26 +138,26 @@ func (c *Client) Recognize(bufWav []byte) (string, error) {
 		if dat == 0 && err == io.EOF {
 			err = c.socket.WriteMessage(websocket.TextMessage, []byte("{\"eof\" : 1}"))
 			if err != nil {
-				c.log.ERROR(err.Error())
+				log.ERROR(err.Error())
 				return "", err
 			}
 			break
 		}
 		if err != nil {
-			c.log.ERROR(err.Error())
+			log.ERROR(err.Error())
 			return "", err
 		}
 
 		err = c.socket.WriteMessage(websocket.BinaryMessage, buf)
 		if err != nil {
-			c.log.ERROR(err.Error())
+			log.ERROR(err.Error())
 			return "", err
 		}
 
 		// Read message from server
 		_, _, err = c.socket.ReadMessage()
 		if err != nil {
-			c.log.ERROR(err.Error())
+			log.ERROR(err.Error())
 			return "", err
 		}
 	}
@@ -151,7 +165,7 @@ func (c *Client) Recognize(bufWav []byte) (string, error) {
 	// Read final message from server
 	_, msg, err := c.socket.ReadMessage()
 	if err != nil {
-		c.log.ERROR(err.Error())
+		log.ERROR(err.Error())
 		return "", err
 	}
 
@@ -161,7 +175,7 @@ func (c *Client) Recognize(bufWav []byte) (string, error) {
 	// Unmarshalling received message
 	err = json.Unmarshal(msg, &m)
 	if err != nil {
-		c.log.ERROR(err.Error())
+		log.ERROR(err.Error())
 		return "", err
 	}
 	return m.Text, nil
--- pkg/vosk-client/vosk-client_test.go
+++ pkg/vosk-client/vosk-client_test.go
@@ -1,27 +1,11 @@
 package voskclient
 
 import (
-	"fmt"
 	"testing"
 )
 
-type log struct {
-}
-
-func (l *log) INFO(v ...string) {
-	fmt.Println(v)
-}
-
-func (l *log) ERROR(v ...string) {
-	fmt.Println(v)
-}
-
-func (l *log) DEBUG(v ...string) {
-	fmt.Println(v)
-}
-
 func TestPostConfigure(t *testing.T) {
-	c := New(&log{})
+	c := New()
 	err := c.PostConfigure()
 	if err != nil {
 		t.Fatal(err)
